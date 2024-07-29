7049d4ae216dc5bc8b4c3fc547189d0e94078caf
midharma <67436543+midharma@users.noreply.github.com>
2024-07-29 20:40:45 +0800

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -11,10 +11,10 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
 )
 
-from DanteMusic import app
-from DanteMusic.misc import SUDOERS
+from UsuMusic import app
+from UsuMusic.misc import SUDOERS
 from .notes import extract_urls
-from DanteMusic.utils.database import is_gbanned_user
+from UsuMusic.utils.database import is_gbanned_user
 
 from utils import (
     del_welcome,
@@ -23,9 +23,9 @@ from utils import (
 )
 
 from utils.error import capture_err
-from DanteMusic.utils.functions import check_format, extract_text_and_keyb
-from DanteMusic.utils.keyboard import ikb
-from DanteMusic.utils.permissions import adminsOnly
+from UsuMusic.utils.functions import check_format, extract_text_and_keyb
+from UsuMusic.utils.keyboard import ikb
+from UsuMusic.utils.permissions import adminsOnly
 
 
 async def handle_new_member(member, chat):
