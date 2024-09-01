64f23518e58155498e1286208b83bdad97e6d166
midharma <67436543+midharma@users.noreply.github.com>
2024-09-02 00:42:47 +0800

    Update ban.py

--- plugins/ban.py
+++ plugins/ban.py
@@ -46,7 +46,6 @@ __HELP__ = """<blockquote><b>
 /purge - Purge Messages
 /purge [n] - Purge "n" number of messages from replied message
 /del - Delete Replied Message
-/staff - Cek admin
 /admin - Promote A Member
 /fulladmin - Promote A Member With All Rights
 /unadmin - Demote A Member
@@ -114,101 +113,6 @@ async def remove_warns(chat_id: int, name: str) -> bool:
         )
         return True
     return False
-
-@app.on_message(
-    filters.command(["staff"]) & filters.group
-)
-async def staff_cmd(_, message: Message):
-    chat_title = message.chat.title
-    creator = []
-    co_founder = []
-    admin = []
-    async for x in message.chat.get_members():
-        mention = f"<a href=tg://user?id={x.user.id}>{x.user.first_name} {x.user.last_name or ''}</a>"
-        if (
-            x.status.value == "administrator"
-            and x.privileges
-            and x.privileges.can_promote_members
-        ):
-            if x.custom_title:
-                co_founder.append(f" ┣ {mention} - {x.custom_title}")
-            else:
-                co_founder.append(f" ┣ {mention}")
-        elif x.status.value == "administrator":
-            if x.custom_title:
-                admin.append(f" ┣ {mention} - {x.custom_title}")
-            else:
-                admin.append(f" ┣ {mention}")
-        elif x.status.value == "owner":
-            if x.custom_title:
-                creator.append(f" ┗ {mention} - {x.custom_title}")
-            else:
-                creator.append(f" ┗ {mention}")
-    if not co_founder and not admin:
-        result = f"""
-<b>Staff Grup
-{chat_title}
-
-👑 Owner:
-{creator[0]}</b>"""
-    elif not co_founder:
-        adm = admin[-1].replace("┣", "┗")
-        admin.pop(-1)
-        admin.append(adm)
-        result = f"""
-<b>Staff Grup
-{chat_title}
-
-👑 Owner:
-{creator[0]}
-
-👮 admin:</b>
-""" + "\n".join(
-            admin
-        )
-    elif not admin:
-        cof = co_founder[-1].replace(" ┣", " ┗")
-        co_founder.pop(-1)
-        co_founder.append(cof)
-        result = f"""
-<b>Staff Grup
-{chat_title}
-
-👑 Owner:
-{creator[0]}
-
-👮 Co-Founder:</b>
-""" + "\n".join(
-            co_founder
-        )
-    else:
-        adm = admin[-1].replace(" ┣", " ┗")
-        admin.pop(-1)
-        admin.append(adm)
-        cof = co_founder[-1].replace(" ┣", " ┗")
-        co_founder.pop(-1)
-        co_founder.append(cof)
-        result = (
-            (
-                f"""
-<b>Staff Grup
-{chat_title}
-
-👑 Owner:
-{creator[0]}
-
-👮 Co-Founder:</b>
-"""
-                + "\n".join(co_founder)
-                + """
-
-<b>👮 admin:</b>
-"""
-            )
-            + "\n".join(admin)
-        )
-
-    await message.reply(result)
 
 
 @app.on_message(filters.command(["kick", "skick"]) & ~filters.private & ~BANNED_USERS)
